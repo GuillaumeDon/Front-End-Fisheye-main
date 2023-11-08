d222035661037c369c5752f9df39cba8f8766424
Guillaume Donizetti <glm.donizetti@gmail.com>
2023-11-08 19:27:39 +0100

    ajout des information des photographes

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,20 +1,39 @@
-function photographerTemplate(data) {
-    const { name, portrait } = data;
 
+
+function photographerTemplate(data) {
+    const { name, portrait, city, tagline, price, country } = data;
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        article.classList.add('main-container-photographer-section-article')
-        const img = document.createElement( 'img' );
-        img.classList.add('main-container-photographer-section-article-img')
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.classList.add('main-container-photographer-section-article-title')
+        const article = document.createElement('article');
+        article.classList.add('main-container-photographer-section-article');
+
+        const img = document.createElement('img');
+        img.classList.add('main-container-photographer-section-article-img');
+        img.setAttribute("src", picture);
+        img.setAttribute("alt", `Portrait of ${name}`);
+
+        const h2 = document.createElement('h2');
+        h2.classList.add('main-container-photographer-section-article-title');
         h2.textContent = name;
+
+        const location = document.createElement('p');
+        location.textContent = city+", "+country;
+
+        const slogan = document.createElement('p');
+        slogan.textContent = tagline;
+
+        const cost = document.createElement('p');
+        cost.textContent = `${price}€/day`;
+
         article.appendChild(img);
         article.appendChild(h2);
-        return (article);
+        article.appendChild(location);
+        article.appendChild(slogan);
+        article.appendChild(cost);
+
+        return article;
     }
-    return { name, picture, getUserCardDOM }
+
+    return { name, picture, getUserCardDOM };
 }
