a1ef3111acff96024cd4d9b923b51472745c5043
Guillaume Donizetti <glm.donizetti@gmail.com>
2023-11-08 15:53:07 +0100

    conversion scss suite

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -5,10 +5,12 @@ function photographerTemplate(data) {
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
-        article.classList.add('')
+        article.classList.add('main-container-photographer-section-article')
         const img = document.createElement( 'img' );
+        img.classList.add('main-container-photographer-section-article-img')
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
+        h2.classList.add('main-container-photographer-section-article-title')
         h2.textContent = name;
         article.appendChild(img);
         article.appendChild(h2);
