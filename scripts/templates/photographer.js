8e5028716d979ee47e30994589cbf1819079b0da
Guillaume Donizetti <glm.donizetti@gmail.com>
2023-11-16 18:44:24 +0100

    ajout de container classement des container articles

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -7,33 +7,47 @@ function photographerTemplate(data) {
         const article = document.createElement('article');
         article.classList.add('main-container-photographer-section-article');
 
+        const id =document.createElement('div');
+        id.classList.add('main-container-photographer-section-article-id');
+
+
+
         const img = document.createElement('img');
-        img.classList.add('main-container-photographer-section-article-img');
+        img.classList.add('main-container-photographer-section-article-id-img');
         img.setAttribute("src", picture);
         img.setAttribute("alt", `Portrait of ${name}`);
 
         const h2 = document.createElement('h2');
-        h2.classList.add('main-container-photographer-section-article-title');
+        h2.classList.add('main-container-photographer-section-article-id-title');
         h2.textContent = name;
 
+
+
+        const content =document.createElement('div');
+        content.classList.add('main-container-photographer-section-article-content');
+
         const location = document.createElement('p');
-        location.classList.add('main-container-photographer-section-article-location');
+        location.classList.add('main-container-photographer-section-article-content-location');
         location.textContent = city+", "+country;
 
         const slogan = document.createElement('p');
-        slogan.classList.add('main-container-photographer-section-article-slogan');
+        slogan.classList.add('main-container-photographer-section-article-content-slogan');
         slogan.textContent = tagline;
 
         const cost = document.createElement('p');
-        cost.classList.add('main-container-photographer-section-article-cost');
+        cost.classList.add('main-container-photographer-section-article-content-cost');
 
         cost.textContent = `${price}€/day`;
 
-        article.appendChild(img);
-        article.appendChild(h2);
-        article.appendChild(location);
-        article.appendChild(slogan);
-        article.appendChild(cost);
+        article.appendChild(id);
+        article.appendChild(content);
+        id.appendChild(img);
+        id.appendChild(h2);
+        content.appendChild(location);
+        content.appendChild(slogan);
+        content.appendChild(cost);
+ 
+ 
 
         return article;
     }
