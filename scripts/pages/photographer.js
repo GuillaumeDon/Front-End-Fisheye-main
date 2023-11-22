c196e5dc922fd41f4c03f538c11953348e6cf53c
Guillaume Donizetti <glm.donizetti@gmail.com>
2023-11-22 18:20:35 +0100

    mise en place choix photographe selon ID

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,2 +1,37 @@
-//Mettre le code JavaScript lié à la page photographer.html
+function getPhotographerIdFromURL() {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    return urlParams.get('id');
+}
 
+async function getPhotographerById(id) {
+    let response = await fetch('../../data/photographers.json');
+    let data = await response.json();
+    return data.photographers.find(p => p.id === parseInt(id));
+}
+
+async function displayPhotographerData(photographer) {
+ 
+
+        const identityElement = document.querySelector('.main-photographer-container-header-infos-identity');
+        const placeElement = document.querySelector('.main-photographer-container-header-infos-place');
+        const taglineElement = document.querySelector('.main-photographer-container-header-infos-txt');
+        const pictureElement = document.querySelector('.main-photographer-container-header-picture');
+    
+       
+        identityElement.textContent = photographer.name;
+        placeElement.textContent = `${photographer.city}, ${photographer.country}`;
+        taglineElement.textContent = photographer.tagline;
+        pictureElement.src = `assets/photographers/${photographer.portrait}`; 
+        pictureElement.alt = `Portrait de ${photographer.name}`;
+    
+    
+}
+
+async function init() {
+    const photographerId = getPhotographerIdFromURL();
+    const photographer = await getPhotographerById(photographerId);
+    displayPhotographerData(photographer);
+}
+
+init();
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,24 +1,32 @@
 
 function photographerTemplate(data) {
-    const { name, portrait, city, tagline, price, country } = data;
+    const { name, portrait, city, tagline, price, country, id } = data;
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
+
+
         const article = document.createElement('article');
         article.classList.add('main-container-photographer-section-article');
 
-        const id =document.createElement('div');
-        id.classList.add('main-container-photographer-section-article-id');
+
+        
+
+        
+       
+
+        const identity =document.createElement('div');
+        identity.classList.add('main-container-photographer-section-article-identity');
 
 
 
         const img = document.createElement('img');
-        img.classList.add('main-container-photographer-section-article-id-img');
+        img.classList.add('main-container-photographer-section-article-identity-img');
         img.setAttribute("src", picture);
         img.setAttribute("alt", `Portrait of ${name}`);
 
         const h2 = document.createElement('h2');
-        h2.classList.add('main-container-photographer-section-article-id-title');
+        h2.classList.add('main-container-photographer-section-article-identity-title');
         h2.textContent = name;
 
 
@@ -39,17 +47,25 @@ function photographerTemplate(data) {
 
         cost.textContent = `${price}€/day`;
 
-        article.appendChild(id);
+        const anchor = document.createElement('a');
+        anchor.href =`photographer.html?id=${id}`;
+        anchor.appendChild(article); 
+   
+
+
+        article.appendChild(identity);
         article.appendChild(content);
-        id.appendChild(img);
-        id.appendChild(h2);
+        identity.appendChild(img);
+        identity.appendChild(h2);
         content.appendChild(location);
         content.appendChild(slogan);
         content.appendChild(cost);
  
- 
+        // anchor.addEventListener('click', function(event) {
+        //     console.log(`Voici l'ID:${id}`);
+        // });
 
-        return article;
+        return anchor;
     }
 
     return { name, picture, getUserCardDOM };
